38626501671320127b068039d1cfec07bd32f01d
tuetran <tue1996@gmail.com>
2024-06-09 20:27:39 +0700

    remove redundant code

--- src/binance-api-controllers/spot-controllers.ts
+++ src/binance-api-controllers/spot-controllers.ts
@@ -3,8 +3,6 @@ import { getPrice } from "./market-controllers";
 
 export async function getSpotBalance(client: Spot) {
   const userAssetResponse = await client.userAsset({ needBtcValuation: true });
-  const tickerResponse = await client.symbolPriceTicker({ symbol: 'BTCUSDT' })
-
   const balanceInBtc = userAssetResponse.map(x => x.btcValuation).reduce((a, b) => a + b);
   const btcPrice = await getPrice(client, 'BTCUSDT')
 
